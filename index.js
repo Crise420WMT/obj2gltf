c65d433fa192d899baa33542a05e32fe1514b423
wuzhihua <wuzhihua@bimsop.com>
2017-03-29 18:53:36 +0800

    add more than 4 vertices render fuc,add if obj file appear '\' character read the next-line and the pre-line into a complete line

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = {
-    convert : require('./lib/convert')
+// module.exports = {
+//     convert : require('./lib/convert')
+// };
+var convert = require('./lib/convert');
+var options = {
+    separate : true // Don't embed textures in the converted glTF
 };
+// convert('./specs/data/archicad/archicad/Untitled2/Untitled.obj', './specs/data/archicad/suibian.gltf')
+// convert('./specs/data/chenzhou/chenzhou.obj', './specs/data/chenzhou/chenzhou.gltf', options)
+// convert('./specs/data/toolBar/Bar Stool 20.obj', './specs/data/toolBar/BarStool20.gltf')
+// convert('./specs/data/cerberus/Cerberus.obj', './specs/data/cerberus/cerberus.gltf')
+// convert('./specs/data/ship_boat/ship_boat.obj', './specs/data/ship_boat/ship_boat.gltf', options)
+convert('./specs/data/waterMachine/水处理机组2.obj', './specs/data/waterMachine/waterMachine.gltf')
+// convert('./specs/data/3man/rar/Man_Pose006.obj', './specs/data/3man/rar/Man_Pose006.gltf', options)
+// convert('./specs/data/archicad/archicad/卡车.obj', './specs/data/archicad/car.gltf')
+// // convert('./specs/data/archicad/archicad/壁柜末端/壁柜末端.obj', './specs/data/archicad/bigui.gltf')
+    .then(function() {
+        // document.write('Converted model success!');
+    });
---
+++ specs/data/ship_boat/fragmentShader3.glsl
@@ -0,0 +1,30 @@
+precision highp float;
+uniform vec4 u_ambient;
+uniform vec4 u_diffuse;
+uniform vec4 u_emission;
+uniform vec4 u_specular;
+uniform float u_shininess;
+uniform float u_transparency;
+varying vec3 v_positionEC;
+varying vec3 v_normal;
+void main(void) {
+  vec3 normal = normalize(v_normal);
+  if (gl_FrontFacing == false)
+  {
+    normal = -normal;
+  }
+  vec4 diffuse = u_diffuse;
+  vec3 diffuseLight = vec3(0.0, 0.0, 0.0);
+  vec3 emission = u_emission.rgb;
+  vec3 ambient = u_ambient.rgb;
+  vec3 viewDir = -normalize(v_positionEC);
+  vec3 ambientLight = vec3(0.0, 0.0, 0.0);
+  ambientLight += vec3(0.2, 0.2, 0.2);
+  vec3 l = vec3(0.0, 0.0, 1.0);
+  diffuseLight += vec3(1.0, 1.0, 1.0) * max(dot(normal,l), 0.);
+  vec3 color = vec3(0.0, 0.0, 0.0);
+  color += diffuse.rgb * diffuseLight;
+  color += emission;
+  color += ambient * ambientLight;
+  gl_FragColor = vec4(color * diffuse.a, diffuse.a * u_transparency);
+}
